3ee952c2a058e9bee5f96ebfc9c978d443cb4a2e
Jack Donaghy <jackwilliamdonaghy@gmail.com>
2026-09-17 14:52:09 +0100

    Part 2 complete

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,41 @@ app.get("/ping", async (_req : Request, res: Response) => { res.json({
 
 }); 
 
- 
+ app.get('/bananas', async (_req : Request, res: Response) => { 
+
+    res.json({ 
+
+    message: "this is bananas"
+    }); 
+
+}); 
+
+ app.get('/games', async (_req : Request, res: Response) => { 
+
+    res.json({ 
+
+    message: "so manyyyy gammmmessssss"
+    ,game1: "this is game 1"
+    ,game2: "this is game 2"
+    ,game3: "this is game 3"
+
+    }); 
+
+}); 
+
+ app.get('/gameBetter', async (_req : Request, res: Response) => { 
+
+    res.json({ 
+
+    message: "so manyyyy gammmmessssss"
+    ,game1: "this is game 1"
+    ,game2: "this is game 2"
+    ,game3: "this is game 3"
+    ,game4: { innergame: "this is inner game 4", innergame2: "this is inner game 4.2" }
+
+    }); 
+
+}); 
 
 app.listen(PORT, () => { 
 
