6c3293c52068a4072e48285ad9994b90576cbb54
Jack Donaghy <jackwilliamdonaghy@gmail.com>
2026-09-17 14:58:52 +0100

    Part 3 Complete

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 400;
 
 const app: Application = express(); 
 
+app.use((req, _res, next) => {   
+
+    console.log(`${req.method} ${req.originalUrl}`); 
+
+    next(); 
+
+}); 
+
  
 
 app.get("/ping", async (_req : Request, res: Response) => { res.json({ 
@@ -59,3 +67,12 @@ app.listen(PORT, () => {
     console.log("Server is running on port", PORT); 
 
     }); 
+
+    app.use((req, _res, next) => {   
+
+    console.log(`${req.method} ${req.originalUrl}`); 
+
+    next(); 
+
+}); 
+
